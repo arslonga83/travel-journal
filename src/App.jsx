5597c9d47a884ae7513b98e6afc3aa9addf2133e
arslonga83 <jlwood83@gmail.com>
2022-10-11 22:59:01 -0700

    setup file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import Header from './Components/Header'
+import Card from './Components/Card'
 import './App.css'
+
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Header />
+      <section>
+        <Card/>
+        <Card/>
+        <Card/>
+      </section>
        
     </div>
   )
---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+function Header () {
+  return (
+    <h1>I'm a header component.</h1>
+  )
+}
+
+export default Header
