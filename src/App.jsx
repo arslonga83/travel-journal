615ef85e98ef3298b6397bd8d4348621043b38d8
arslonga83 <jlwood83@gmail.com>
2022-10-12 11:56:08 -0700

    put cards in responsive columns and stick footer to bottom

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <section>
+      <section className='card-container'>
         <Card/>
         <Card/>
         <Card/>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,7 +12,7 @@ function Card () {
         </div>
         <h1>Location Name</h1>
         <h3>1 Jan, 2022 - 31 Jan, 2022</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis natus magni obcaecati adipisci eius. Quaerat excepturi molestias dolores possimus aperiam delectus corporis quibusdam laborum assumenda temporibus? Minima sequi nam nulla!</p>
+        <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis natus magni obcaecati adipisci eius. Quaerat excepturi molestias dolores possimus aperiam delectus corporis quibusdam laborum assumenda temporibus? Minima sequi nam nulla!</p>
       </div>
       
     </div>
