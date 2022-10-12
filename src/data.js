ba08e70ecb930c833092e70755ca829b9cfb39b8
arslonga83 <jlwood83@gmail.com>
2022-10-12 13:39:21 -0700

    lock image size

--- src/data.js
+++ src/data.js
@@ -9,30 +9,30 @@ export default [
       imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
   },
   {
-    title: "Taj Mahal",
-    location: "India",
-    googleMapsUrl: "https://goo.gl/maps/E83qBcyhkp7Kcn3X9",
-    startDate: "04 July, 2019",
-    endDate: "12 July, 2019",
-    description: "The Taj Mahal was designated as a UNESCO World Heritage Site in 1983 for being the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage. It is regarded by many as the best example of Mughal architecture and a symbol of India's rich history. The Taj Mahal attracts more than 6 million visitors a year and in 2007, it was declared a winner of the New 7 Wonders of the World initiative.",
-    imageUrl: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2942&q=80"
-},
-{
-  title: "Busan",
-  location: "Korea",
-  googleMapsUrl: "https://goo.gl/maps/tEv5eJeAjBbb2j7C8",
-  startDate: "11 Mar, 2020",
-  endDate: "24 Mar, 2020",
-  description: "Busan, officially known as Busan Metropolitan City, is South Korea's second-most populous city after Seoul, with a population of over 3.4 million inhabitants. Formerly romanized as Pusan, it is the economic, cultural and educational center of southeastern South Korea, with its port being Korea's busiest and the sixth-busiest in the world.[a] The surrounding Southeast Economic Zone is South Korea's largest industrial area",
-  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/39/Haedong_Yonggungsa%2C_Busan.jpg"
-},
-{
-  title: "The Space Needle",
-  location: "United States",
-  googleMapsUrl: "https://goo.gl/maps/kcig7abtTS7FAVwy7",
-  startDate: "22 Dec, 2022",
-  endDate: "23 Dec, 2022",
-  description: "The Space Needle is an observation tower in Seattle, Washington, United States. Considered to be an icon of the city, it has been designated a Seattle landmark. Located in the Lower Queen Anne neighborhood, it was built in the Seattle Center for the 1962 World's Fair, which drew over 2.3 million visitors.",
-  imageUrl: "https://images.unsplash.com/photo-1542223616-9de9adb5e3e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BhY2UlMjBuZWVkbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-} 
+      title: "Taj Mahal",
+      location: "India",
+      googleMapsUrl: "https://goo.gl/maps/E83qBcyhkp7Kcn3X9",
+      startDate: "04 July, 2019",
+      endDate: "12 July, 2019",
+      description: "The Taj Mahal was designated as a UNESCO World Heritage Site in 1983 for being the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage. It is regarded by many as the best example of Mughal architecture and a symbol of India's rich history. The Taj Mahal attracts more than 6 million visitors a year and in 2007, it was declared a winner of the New 7 Wonders of the World initiative.",
+      imageUrl: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2942&q=80"
+  },
+  {
+      title: "Busan",
+      location: "Korea",
+      googleMapsUrl: "https://goo.gl/maps/tEv5eJeAjBbb2j7C8",
+      startDate: "11 Mar, 2020",
+      endDate: "24 Mar, 2020",
+      description: "Busan, officially known as Busan Metropolitan City, is South Korea's second-most populous city after Seoul, with a population of over 3.4 million inhabitants. Formerly romanized as Pusan, it is the economic, cultural and educational center of southeastern South Korea, with its port being Korea's busiest and the sixth-busiest in the world.[a] The surrounding Southeast Economic Zone is South Korea's largest industrial area",
+      imageUrl: "https://images.unsplash.com/photo-1596616604802-a321eb13a070?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+      title: "The Space Needle",
+      location: "United States",
+      googleMapsUrl: "https://goo.gl/maps/kcig7abtTS7FAVwy7",
+      startDate: "22 Dec, 2022",
+      endDate: "23 Dec, 2022",
+      description: "The Space Needle is an observation tower in Seattle, Washington, United States. Considered to be an icon of the city, it has been designated a Seattle landmark. Located in the Lower Queen Anne neighborhood, it was built in the Seattle Center for the 1962 World's Fair, which drew over 2.3 million visitors.",
+      imageUrl: "https://images.unsplash.com/photo-1542223616-9de9adb5e3e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BhY2UlMjBuZWVkbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+  }
 ]
